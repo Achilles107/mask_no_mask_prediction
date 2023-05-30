e35eb2debd1423305f2ffcca6bffe37980b12952
Abhinav Shukla <abhinavshukla835@gmail.com>
2023-05-30 01:51:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def hello_world():
             return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT', 3000))
+    app.run(host="0.0.0.0", debug=True, port=os.getenv('PORT', 3000))
